f7771880e3a263a7f95be17d3529188881512e65
Meet Kumar Jain <meetkjain83@gmail.com>
2023-08-20 14:53:42 +0530

    Coding further the entire implementation

--- blocks/lhs-home/lhs-home.js
+++ blocks/lhs-home/lhs-home.js
@@ -2,11 +2,18 @@ import { getAllSheetData } from '../../scripts/scripts.js';
 
 
 function createCard(row, style) {
+    console.log(row.hostname);
     const card = document.createElement('div');
     if (style) card.classList.add(style);
     const cardContent = document.createElement('div');
     cardContent.classList.add('card-content');
     card.append(cardContent)
+    const cardHostname = document.createElement('div');
+    const cardMetadata = document.createElement('div');
+    cardContent.append(cardHostname);
+    cardHostname.after(cardMetadata);
+    let str = row.hostname.charAt(0).toUpperCase() + row.hostname.slice(1);
+    cardHostname.innerHTML = `<h2>${str}</h2>`;
     const cardScore = document.createElement('div');
     cardScore.classList.add('card-score');
     card.append(cardScore);
