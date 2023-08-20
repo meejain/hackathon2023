bd2bec042acf6a1cbddac403f5be32aeeaa07411
Meet Kumar Jain <meetkjain83@gmail.com>
2023-08-20 16:27:10 +0530

    Coding further the entire implementation

--- blocks/lhs-home/lhs-home.js
+++ blocks/lhs-home/lhs-home.js
@@ -2,7 +2,7 @@ import { getAllSheetData } from '../../scripts/scripts.js';
 
 
 function createCard(row, style) {
-    console.log(row.hostname);
+    console.log(row.skipmin);
     const card = document.createElement('div');
     if (style) card.classList.add(style);
     const cardContent = document.createElement('div');
@@ -14,6 +14,14 @@ function createCard(row, style) {
     cardHostname.after(cardMetadata);
     let str = row.hostname.charAt(0).toUpperCase() + row.hostname.slice(1);
     cardHostname.innerHTML = `<h2>${str}</h2>`;
+    cardMetadata.innerHTML= `<h4>Segment Store min. value when OnRC was skipped: ${row.skipmin}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Segment Store max. value when OnRC was skipped: ${row.skipmax}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Segment Store min. value when OnRC was actually run: ${row.postmin}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Segment Store max. value when OnRC was actually run: ${row.postmax}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Segment Store latest value from Splunk if OnRC was skipped: ${row.lastlogskip}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Segment Store latest value from Splunk if OnRC actually ran: ${row.lastlogpost}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Segment Store latest value from OffRc log: ${row.lastoffRclog}</h4>`;
+    cardMetadata.innerHTML= cardMetadata.innerHTML + `<h4>Current Segment Store size is: ${row.current}</h4>`;
     const cardScore = document.createElement('div');
     cardScore.classList.add('card-score');
     card.append(cardScore);
